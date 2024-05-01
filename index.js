2f807ddc201fa32285f0082b4c497bfd7be47711
Rahul Jain <rahuljain820436@gmail.com>
2024-05-01 23:05:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.listen(PORT, () => {
 })
 
 app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
+  res.send('Hey this is my API running Rahul')
 })
 
 app.get('/about', (req, res) => {
